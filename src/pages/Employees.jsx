c9a71d0f7e1541a3bfe301999707b44cd02cfa87
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-14 08:45:47 +0800

    Employees Table

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Inject, Search, Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, contextMenuItems, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
+  // const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div>Employees</div>
-  )
-}
-
-export default Employees
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
+export default Employees;
