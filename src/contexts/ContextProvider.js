215b98b547c127acdffae58c712fd2a469b7bea3
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-05 23:37:47 +0800

    Theme Setting Functionality - (Backend) (Important) Load States into Context

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,21 @@ export const ContextProvider = ({ children }) => {
 	const [activeMenu, setActiveMenu] = useState(true)
 	const [isClicked, setIsClicked] = useState(initialState)
 	const [screenSize, setScreenSize] = useState(undefined)
+	const [currentColor, setCurrentColor] = useState('#03C9D7')
+	const [currentMode, setCurrentMode] = useState('Light')
+	const [themeSettings, setThemeSettings] = useState(false)
+	
+	const setMode = (e) => {
+		setCurrentMode(e.target.value)
+
+		localStorage.setItem('themeMode', e.target.value)
+	}
+
+	const setColor = (e) => {
+		setCurrentColor(e.target.value)
+
+		localStorage.setItem('colorMode', e.target.value)
+	}
 
 	const handleClick = (clicked) => {
 		setIsClicked({ ...initialState, [clicked]:true })
@@ -24,7 +39,13 @@ export const ContextProvider = ({ children }) => {
 				activeMenu, setActiveMenu,
 				isClicked, setIsClicked,
 				handleClick,
-				screenSize, setScreenSize
+				screenSize, setScreenSize,
+
+				// for Theme Colors
+				currentMode, setCurrentMode,
+				currentColor, setCurrentColor,
+				//for Theme Setting
+				themeSettings, setThemeSettings
 			}}
 		>
 			{children}
