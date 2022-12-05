bc32e0347b107339e6911c3f114da6d742d4cd14
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-05 17:56:06 +0800

    Theme: Close Button UI

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
 // import { MdOutlineCancel } from 'react-icons/md';
 // import { BsCheck } from 'react-icons/bs';
 // import { themeColors } from './data/dummy';
@@ -6,8 +7,19 @@ import React from 'react'
 const ThemeSettings = () => {
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400"></div>
-      tsett
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
+        <div className="flex justify-between items-center p-4 ml-4"> 
+          <p className='font-semibold text-lg'>Settings</p>
+          <button type="button" 
+            onClick={() => {}}
+            style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%'}} // the react button size
+            className="text-2xl p-3 hover:bg-light-gray" // the shading
+            
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+      </div>
 
     </div>
   )
