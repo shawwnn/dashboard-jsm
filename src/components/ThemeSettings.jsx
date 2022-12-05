cb92e17f09010acc0ce1327c7c5a1c46620f2be2
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-05 23:03:11 +0800

    UI for Theme Dark or Light

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -9,7 +9,7 @@ const ThemeSettings = () => {
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
       <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
         <div className="flex justify-between items-center p-4 ml-4"> 
-          <p className='font-semibold text-lg'>Settings</p>
+          <p className='font-semibold text-xl'>Settings</p>
           <button type="button" 
             onClick={() => {}}
             style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%'}} // the react button size
@@ -18,6 +18,30 @@ const ThemeSettings = () => {
           >
             <MdOutlineCancel />
           </button>
+        </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Options</p>
+
+          <div className="mt-4">
+            <input type="radio" id="light" name="theme" value="Light" className="cursor-pointer"
+              onClick={() => {}} 
+              checked={true}
+            />
+            <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+              Light
+            </label>
+          </div>
+
+          <div className="mt-4">
+            <input type="radio" id="dark" name="theme" value="Dark" className="cursor-pointer"
+              onClick={() => {}} 
+              checked={true}
+            />
+            <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
+              Dark
+            </label>
+          </div>
         </div>
       </div>
 
